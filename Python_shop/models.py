19b724e544cbb5c4a5527dcc1775f3962dfebd79
Arthur Mirsaitov <arthurheelton@gmail.com>
2018-01-18 13:00:15 +0300

    Add models

--- Python_shop/models.py
+++ Python_shop/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.base_user import AbstractBaseUser
 
 class Items(models.Model):   # Items = Goods
 
-   section = models.ForeignKey('Category', related_name='items') # Category = Section
    item_name = models.CharField(max_length=140)    #name = item_name
+   section = models.ForeignKey('Category', related_name='items')  # Category = Section
 
     def __str__(self):
         return self.item_name
@@ -20,14 +20,14 @@ class Section(models.Model):
 
 class Items_options(models.Model):        #Goods_Variation = Items_options
 
-    parent = models.ForeignKey('Item', related_name="item")
     option_name = models.CharField(max_length=140)       #variation_name = option_name
-    item_description = models.TextField(blank=True)           #description = item_description
     price = models.DecimalField(max_digits=8, decimal_places=2)      #cost = price
+    item_description = models.TextField(blank=True)  # description = item_description
+    parent = models.ForeignKey('Item', related_name="item")
 
     def __str__(self):
         return self.parent.option_name + " " + self.option_name
 
 class Shop_client(AbstractBaseUser):       #Customer = Shop_client
     user = models.CharField(max_length=100) #Username = user
-    email = models.EmailField(max_length=100)  #unique=True
+    email = models.EmailField(max_length=100, unique=True)  #unique=True
